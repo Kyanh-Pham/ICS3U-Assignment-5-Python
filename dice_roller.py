b2b1865918efc5703774f16add39441c55e8b9ab
Kyanh Pham <kyanh.pham@mths.ca>
2022-11-17 23:49:15 +0000

    finished

--- dice_roller.py
+++ dice_roller.py
@@ -9,19 +9,24 @@ import random
 def main():
 
     loop_counter = 0
-    dice_1 = random.randint(0, 6)
-    dice_2 = random.randint(0, 6)
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
 
-    # Input, Process and Output
+    # Process and Output
     while dice_1 != dice_2:
-        dice_1 = random.randint(0, 6)
-        dice_2 = random.randint(0, 6)
+        dice_1 = random.randint(1, 6)
+        dice_2 = random.randint(1, 6)
         loop_counter = loop_counter + 1
         if dice_1 == dice_2:
             break
-        print("Roll #{0}...Dice 1 rolled a {1} and dice 2 rolled a {2}!"
-        .format(loop_counter, dice_1, dice_2))
-    print("Roll #{0}... hit doubles! Dice 1 and 2 are {1}!".format(loop_counter, dice_1))
+        print(
+            "Roll #{0}...Dice 1 rolled a {1} and dice 2 rolled a {2}!".format(
+                loop_counter, dice_1, dice_2
+            )
+        )
+    print(
+        "Roll #{0}... hit doubles! Dice 1 and 2 are {1}!".format(loop_counter, dice_1)
+    )
 
     print("\nDone.")
 
